29255a3a67502afe5ed53fea65caf4ab494ba2ae
Jitendrasinh Rajparmar <125237946+Jitendra7666@users.noreply.github.com>
2024-06-14 10:35:25 +0530

    Revert "Change Object name"
    This reverts commit 841bb6b42731b9803da7b6a41d4cb8bf9847623c.

--- src/LoginOperation.java
+++ src/LoginOperation.java
@@ -10,11 +10,11 @@ public class LoginOperation
         driver.manage().window().maximize();
         driver.get("https://the-internet.herokuapp.com/login");
 
-        WebElement txtusername1 = driver.findElement(By.id("username"));
-        txtusername1.sendKeys("admin");
+        WebElement txtusername = driver.findElement(By.id("username"));
+        txtusername.sendKeys("admin");
 
-        WebElement txtpassword2 = driver.findElement(By.id("password"));
-        txtpassword2.sendKeys("Admin12");
+        WebElement txtpassword = driver.findElement(By.id("password"));
+        txtpassword.sendKeys("Admin12");
 
 
         Thread.sleep(2000);
